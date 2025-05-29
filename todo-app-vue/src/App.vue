5df52dab0c4934e77b22a79192a28d2046e14b91
FernandaMoreta <moretafernanda12@gmail.com>
2025-05-29 20:44:03 +0200

    Creacion del pie de pagina y de la pagina NotFound

--- todo-app-vue/src/App.vue
+++ todo-app-vue/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Nav from './components/Nav.vue'
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <div id="app">
     <Nav />
     <router-view />
+    <Footer />
   </div>
   
 </template>
---
+++ todo-app-vue/src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <footer class="app-footer">
+    <p>&copy; 2024 ToDo List App FM. Todos los derechos reservados.</p>
+    <p>Desarrollado por <a href="https://github.com/FernandaMoreta" target="_blank">Fernanda Moreta</a></p>
+    </footer>
+</template>
